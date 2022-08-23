23051719af306387b1d74a5a53c1d19048004a90
soulofjava <isamaulanatantra@gmail.com>
2022-08-23 13:33:27 +0700

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
       'ubuntu': ['Ubuntu', 'sans'],
     },
+     screens: {
+      'hp': '390px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [],
 }
